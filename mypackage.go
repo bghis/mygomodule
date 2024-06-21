ba856d9d9aaf6617288518653f4ef4249780b814
Benjamin Ghis <benjamin.ghis@gmail.com>
2024-06-21 16:06:50 -0700

    add packages 1 and 2

--- mypackage.go
+++
@@ -1,7 +0,0 @@
-package mypackage
-
-import "fmt"
-
-func PrintTest() {
-	fmt.Println("Hello World !")
-}
---
+++ package2/mypackage.go
@@ -0,0 +1,7 @@
+package package1
+
+import "fmt"
+
+func PrintTest() {
+	fmt.Println("You are in package 2")
+}
